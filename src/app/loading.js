5f89afa46373ad2251afbaa365c0dbcfcad29f78
asifikbal09 <asif.ikbal662@gmail.com>
2025-11-26 17:02:17 +0600

    [DONE] group routing link component not found file loading file.

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center text-red-500'>Loading...</h1>
+        </div>
+    );
+};
+
+export default Loading;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -6,8 +6,8 @@ const Counter = () => {
     return (
         <div>
             <h1>Counter: {counter}</h1>
-            <button className='border p-5 m-1.5' onClick={() => setCounter(counter + 1)}>Increment</button>
-            <button className='border p-5' onClick={() => setCounter(counter - 1)}>Decrement</button>
+            <button className="btn btn-accent" onClick={() => setCounter(counter + 1)}>Increment</button>
+            <button className="btn btn-secondary" onClick={() => setCounter(counter - 1)}>Decrement</button>
         </div>
     );
 };
