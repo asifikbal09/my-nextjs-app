26e7a179e1ee3a3e663038eb3efb2729ab83c671
asifikbal09 <asif.ikbal662@gmail.com>
2025-11-26 13:45:40 +0600

    [DONE] Next js Basic Routing and Nested Routing implement.

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>This is AboutPage route</h1>
+    </div>
+  )
+};
+export default AboutPage;
---
+++ src/app/news/artical/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Artical = () => {
+    return (
+        <div>
+            <h1>This is Artical route</h1>
+        </div>
+    );
+};
+
+export default Artical;
